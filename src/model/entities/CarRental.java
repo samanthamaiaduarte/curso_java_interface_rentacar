930709dc3228e62b3fefb02af65b9b718ff77716
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-04-01 20:55:04 -0300

    Creating Invoice, editing CarRental

--- src/model/entities/CarRental.java
+++ src/model/entities/CarRental.java
@@ -7,11 +7,16 @@ public class CarRental {
 	private LocalDateTime start;
 	private LocalDateTime finish;
 	
+	private Vehicle vehicle;
+	private Invoice invoice;
+	
 	public CarRental() { }
 
-	public CarRental(LocalDateTime start, LocalDateTime finish) {
+	public CarRental(LocalDateTime start, LocalDateTime finish, Vehicle vehicle, Invoice invoice) {
 		this.start = start;
 		this.finish = finish;
+		this.vehicle = vehicle;
+		this.invoice = invoice;
 	}
 
 	public LocalDateTime getStart() {
@@ -29,5 +34,21 @@ public class CarRental {
 	public void setFinish(LocalDateTime finish) {
 		this.finish = finish;
 	}
-	
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+
 }
